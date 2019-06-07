c689387558894aa89f5efef98a2a1e1350444f6c
jonathandarwin <jonathanjojo38@yahoo.com>
2019-06-07 18:13:29 +0700

    initial commit

---
+++ 3_counting_circles_ellipses.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('blobs.jpg', 0)
+cv2.imshow('Original Image', image)
+cv2.waitKey(0)
+
+# create detector
+detector = cv2.SimpleBlobDetector_create()
+
+# detect blobs
+kp = detector.detect(image)
+
+# draw blobs
+blank = np.zeros((1,1))
+blobs = cv2.drawKeypoints(image, kp, blank, (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+numberOfBlobs = len(kp)
+text = 'Total number of blobs : ' + str(numberOfBlobs)
+cv2.putText(blobs, text, (20, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 0, 255), 2)
+
+# display image with blob keypoints
+cv2.imshow('Blob using default parameters' ,blobs)
+cv2.waitKey(0)
+
+# set parameters
+params = cv2.SimpleBlobDetector_Params()
+
+params.filterByArea = True
+params.minArea = 100
+
+params.filterByCircularity = True
+params.minCircularity = 0.9
+
+params.filterByConvexity = False
+params.minConvexity = 0.2
+
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+# create detector
+detector = cv2.SimpleBlobDetector_create(params)
+
+# detect blobs
+kp = detector.detect(image)
+
+# draw blobs
+blank = np.zeros((1,1))
+blobs = cv2.drawKeypoints(image, kp, blank, (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+numberOfBlobs = len(kp)
+text = 'Number of circular blobs : ' + str(numberOfBlobs)
+cv2.putText(blobs, text, (20, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 0, 255), 2)
+
+cv2.imshow('Filtering Circular Blobs Only', blobs)
+cv2.waitKey(0)
+cv2.destrotAllWindows()
